4ce8575183231bdc6fd8b6e3ca0d31c28aa20aa7
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-19 00:16:43 +0600

    fixed conflict route bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app=express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
@@ -33,6 +33,12 @@ async function run() {
     const toysCollection = client.db("toysStore").collection("carToys");
 app.get('/toys',async(req,res)=>{
     const result = await toysCollection.find().toArray()
+    res.send(result)
+})
+app.get('/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await toysCollection.findOne(query)
     res.send(result)
 })
 // api for tab implement
