d92ca1f24fe7269cdf7ccb9438ecd3f920867e9d
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-20 22:17:46 +0600

    change my toys api route

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const app=express();
-require('dotenv').config()
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-res.send("server is running")
-})
-
-
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njyz70v.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,114 +29,128 @@ async function run() {
     // await client.connect();
 
     const toysCollection = client.db("toysStore").collection("carToys");
-const indexKeys = {toyName:1,price:1}
-const indexOptions = {name:"toyNameprice"}
-const result = await toysCollection.createIndex(indexKeys,indexOptions)
+    // const indexKeys = { toyName: 1, price: 1 };
+    // const indexOptions = { name: "toyNameprice" };
+    // const result = await toysCollection.createIndex(indexKeys, indexOptions);
 
-
-app.get('/toys',async(req,res)=>{
-    const result = await toysCollection.find().sort({ price: 1 }).limit(20).toArray()
-    res.send(result)
-})
-app.get('/toys/home/:id',async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await toysCollection.findOne(query)
-    res.send(result)
-})
-// api for tab implement
-app.get('/toys/:text',async(req,res)=>{
-    // console.log(req.params.text);
-    if(req.params.text == "bike"){
-        const result =await toysCollection.find({category:req.params.text}).sort({ price: 1 }).toArray()
+    app.get("/toys", async (req, res) => {
+      const result = await toysCollection
+        .find()
+        .sort({ price: 1 })
+        .limit(20)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/toys/home/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+    // api for tab implement
+    app.get("/toys/:text", async (req, res) => {
+      // console.log(req.params.text);
+      if (req.params.text == "bike") {
+        const result = await toysCollection
+          .find({ category: req.params.text })
+          .sort({ price: 1 })
+          .toArray();
         // console.log(result);
-        res.send(result)
-    }else if(req.params.text == "mini-train"){
-        const result =await toysCollection.find({category:req.params.text}).sort({ price: 1 }).toArray()
+        res.send(result);
+      } else if (req.params.text == "mini-train") {
+        const result = await toysCollection
+          .find({ category: req.params.text })
+          .sort({ price: 1 })
+          .toArray();
         // console.log(result);
-        res.send(result)  
-    }else{
-        const result =await toysCollection.find({category:req.params.text}).sort({ price: 1 }).toArray()
+        res.send(result);
+      } else {
+        const result = await toysCollection
+          .find({ category: req.params.text })
+          .sort({ price: 1 })
+          .toArray();
         // console.log(result);
-        res.send(result) 
-    }
-  
-})
+        res.send(result);
+      }
+    });
 
-// api for sorting
-app.get('/sortby/:text',async(req,res)=>{
-  // console.log(req.params.text);
-  if(req.params.text == "acending"){
-      const result =await toysCollection.find().sort({ price: 1 }).toArray()
-      // console.log(result);
-      res.send(result)
-  }else{
-      const result =await toysCollection.find().sort({ price: -1 }).toArray()
-      // console.log(result);
-      res.send(result) 
-  }
+    // api for sorting
+    app.get("/sortby/:text", async (req, res) => {
+      console.log(req.params.text);
+      if (req.params.text == "acending") {
+        const result = await toysCollection.find().sort({ price: 1 }).toArray();
+        // console.log(result);
+        res.send(result);
+      } else {
+        const result = await toysCollection
+          .find()
+          .sort({ price: -1 })
+          .toArray();
+        // console.log(result);
+        res.send(result);
+      }
+    });
+    // myToys Api
+    
+   
+    app.get("/myToys/:email", async (req, res) => {
+      console.log(req.params.email);
+      const query = { sellerEmail: req.params.email };
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+    // api for search
+    app.get("/toys/searchbyToy/:text", async (req, res) => {
+      const text = req.params.text;
 
-})
-// myToys Api
-app.get('/allToys/:email',async(req,res)=>{
-  console.log(req.params.email);
-  const query ={sellerEmail:req.params.email}
-  const result= await toysCollection.find(query).toArray()
-  res.send(result)
-})
+      const result = await toysCollection
+        .find({
+          $or: [{ toyName: { $regex: text, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+    // post method
+    app.post("/post-toys", async (req, res) => {
+      const body = req.body;
+      // console.log(body);
+      const result = await toysCollection.insertOne(body);
+      res.send(result);
+    });
 
-// api for search
-app.get('/toys/searchbyToy/:text',async(req,res)=>{
-  const text =req.params.text;
-  
+    // update a toy
+    app.patch("/post-toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateToy = req.body;
+      console.log(updateToy);
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
 
-  const result = await toysCollection.find({
-    $or: [
-      { toyName: { $regex: text, $options: "i" } },
-    ],
-  }).toArray()
-  res.send(result)
-})
-// post method
-app.post('/post-toys',async(req,res)=>{
-  const body=req.body;
-  // console.log(body);
-  const result = await toysCollection.insertOne(body);
-  res.send(result)
-})
+      const updateDoc = {
+        $set: {
+          description: updateToy.description,
+          price: updateToy.price,
+          quantity: updateToy.quantity,
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updateDoc, option);
+      res.send(result);
+    });
 
-// update a toy
-app.patch('/post-toys/:id',async(req,res)=>{
-  const id =req.params.id;
-  const updateToy=req.body;
-  console.log(updateToy);
-  const filter = {_id: new ObjectId(id)}
-  const option ={upsert:true}
+    // delete toy
 
-  const updateDoc = {
-    $set: {
-      
-      description:updateToy.description,
-      price:updateToy.price,
-      quantity:updateToy.quantity,
-    },
-  };
-  const result = await toysCollection.updateOne(filter, updateDoc,option);
-  res.send(result)
-})
-
-// delete toy
-
-app.delete('/post-toys/:id',async(req,res)=>{
-  const id =req.params.id;
-  const query = {_id: new ObjectId(id)}
-  const result = await toysCollection.deleteOne(query)
-   res.send(result)
-})
+    app.delete("/post-toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -146,9 +158,6 @@ app.delete('/post-toys/:id',async(req,res)=>{
 }
 run().catch(console.dir);
 
-
-
-
-app.listen(port,()=>{
-    console.log(`Speedy car toys server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Speedy car toys server is running on port ${port}`);
+});
